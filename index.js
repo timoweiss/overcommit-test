98c12f7028305a6213d147e52fa7613e7aa5ec24
Timo Weiß <timoweiss@Timo-MBP.local>
2014-07-02 11:18:28 +0200

    jshint test

---
+++ index.js
@@ -0,0 +1,16 @@
+// Hello.
+//
+// This is JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+//
+// To start, simply enter some JavaScript anywhere on this page. Your
+// report will appear on the right side.
+//
+// Additionally, you can toggle specific options in the Configure
+// menu.
+
+function main() {
+  return 'Hello, World!';
+}
+a = 1;
+main();
